f781b445a9a92af48552fbd4aac857a4fb168780
unknown <lucas56hoseok@gmail.com>
2025-06-07 10:39:45 -0300

    iniciando projeto

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace HttpProjeto
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
